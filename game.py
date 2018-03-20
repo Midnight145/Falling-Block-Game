2c1b812791ee3057aff7bb7faf67ca1ded7c6d17
Ryan O'Dell <rubikscubeboy27@gmail.com>
2018-03-20 17:03:51 -0500

    im an idiot

--- game.py
+++ game.py
@@ -20,12 +20,12 @@ def draw_at_bottom():
 while not game_over:
         # each second of game
 
-        # if time.time() - game_clock >= 1:
-        #     game_clock = time.time()
-        #     current_piece.move_down()
-        #     if current_piece.at_bottom:
-        #         pieces_at_bottom.append(current_piece)
-        #         current_piece = Piece(random.choice(piece_list))
+        if time.time() - game_clock >= 1:
+            game_clock = time.time()
+            current_piece.move_down()
+            if current_piece.at_bottom:
+                pieces_at_bottom.append(current_piece)
+                current_piece = Piece(random.choice(piece_list))
 
         # start of event handling
         for event in pygame.event.get():
