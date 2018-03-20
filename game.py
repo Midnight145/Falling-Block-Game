7607d09dcac86ee41484dd083e2dd1e15a85ae51
Ryan O'Dell <rubikscubeboy27@gmail.com>
2018-03-20 10:58:13 -0500

    added t piece

--- game.py
+++ game.py
@@ -7,13 +7,13 @@ game_over = False
 
 
 game_clock = time.time()
-current_piece = Piece('i')
+current_piece = Piece('t')
 
 while not game_over:
         # each second of game
         if time.time() - game_clock >= 1:
             game_clock = time.time()
-            current_piece.y += 20
+            current_piece.move_down()
 
         # start of event handling
         for event in pygame.event.get():
@@ -27,16 +27,10 @@ while not game_over:
                         current_piece.rotate()
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    if current_piece.x <= 20:
-                        pass
-                    else:
-                        current_piece.x -= 20
+                    current_piece.move_left()
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
-                    if current_piece.x >= 220 - current_piece.width:
-                        pass
-                    else:
-                        current_piece.x += 20
+                    current_piece.move_right()
 
         screen.fill((0, 0, 0))
         current_piece.draw(screen)
--- piece_functions/piece.py
+++ piece_functions/piece.py
@@ -4,21 +4,36 @@ import pygame
 class Piece:
     def __init__(self, piece):
         self.y = 0
+        self.x = 100
         self.piece = piece
+
         if piece == 'i':
             self.color = (0, 0, 255)
             self.width = 20
             self.height = 80
-            self.x = 100
+
         elif piece == 'o':
             self.color = (0, 255, 0)
             self.width = 40
             self.height = 40
-            self.x = 100
+
+        elif piece == 't':
+            self.color = (255, 128, 0)
+            # self.points = ([100, 0], [100, 60], [120, 20], [120, 40], [120, 60], [120, 0])
+            self.width = 20
+            self.height = 60
+            self.y1 = 20
+            self.x1 = 120
+            self.width1 = 20
+            self.height1 = 20
 
     def draw(self, screen):
         if self.piece == 'i' or self.piece == 'o':
             pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
+        elif self.piece == 't':
+            # pygame.draw.polygon(screen, self.color, self.points, self.width)
+            pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
+            pygame.draw.rect(screen, self.color, pygame.Rect(self.x1, self.y1, self.width1, self.height1))
 
     def rotate(self):
         if self.piece == "i":
@@ -26,3 +41,37 @@ class Piece:
             return self.width, self.height
         elif self.piece == 'o':
             pass
+
+    def move_left(self):
+        if self.piece == 'i' or self.piece == 'o':
+            if self.x <= 20:
+                pass
+            else:
+                self.x -= 20
+
+        if self.piece == 't':
+            if self.x <= 20:
+                pass
+            else:
+                self.x -= 20
+                self.x1 -= 20
+
+    def move_right(self):
+        if self.piece == 'i' or self.piece == 'o':
+            if self.x >= 220 - self.width1:
+                pass
+            else:
+                self.x += 20
+        if self.piece == 't':
+            if self.x1 >= 220 - self.width1:
+                pass
+            else:
+                self.x += 20
+                self.x1 += 20
+
+    def move_down(self):
+        if self.piece == 'i' or self.piece == 'o':
+            self.y += 20
+        elif self.piece == 't':
+            self.y += 20
+            self.y1 += 20
