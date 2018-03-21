c9de86e22e8580ec9d41d73d1b4eec8003eb08ad
Ryan O'Dell <rubikscubeboy27@gmail.com>
2018-03-20 19:40:36 -0500

    Added J and L pieces, broke some stuff

--- game.py
+++ game.py
@@ -8,9 +8,13 @@ pieces_at_bottom = []
 pygame.init()
 screen = pygame.display.set_mode((240, 440))
 game_over = False
-current_piece = Piece('t')
+# current_piece = Piece(random.choice(piece_list))
+
+current_piece = Piece('l')
 
 game_clock = time.time()
+
+debug = True
 
 
 def draw_at_bottom():
@@ -19,13 +23,13 @@ def draw_at_bottom():
 
 while not game_over:
         # each second of game
-
-        if time.time() - game_clock >= 1:
-            game_clock = time.time()
-            current_piece.move_down()
-            if current_piece.at_bottom:
-                pieces_at_bottom.append(current_piece)
-                current_piece = Piece(random.choice(piece_list))
+        if not debug:
+            if time.time() - game_clock >= 1:
+                game_clock = time.time()
+                current_piece.move_down()
+                if current_piece.at_bottom:
+                    pieces_at_bottom.append(current_piece)
+                    current_piece = Piece(random.choice(piece_list))
 
         # start of event handling
         for event in pygame.event.get():
--- piece_functions/piece.py
+++ piece_functions/piece.py
@@ -1,7 +1,5 @@
 import pygame
 
-pieces_at_bottom = []
-
 
 class Piece:
     def __init__(self, piece):
@@ -14,6 +12,7 @@ class Piece:
             self.color = (45, 254, 254)
             self.width = 20
             self.height = 80
+            self.rotated = False
 
         elif piece == 'o':
             self.color = (255, 253, 56)
@@ -51,19 +50,46 @@ class Piece:
             self.height1 = 40
             self.rotated = False
 
+        elif piece == 'j':
+            self.color = (11, 36, 251)
+            self.width = 20
+            self.height = 60
+            self.x1 = 120
+            self.y1 = 0
+            self.width1 = 20
+            self.height1 = 20
+            self.rotate_amount = 0
+
+        elif piece == 'l':
+            self.color = (253, 164, 41)
+            self.width = 20
+            self.height = 60
+            self.x1 = 100
+            self.x = 120
+            self.y1 = 0
+            self.width1 = 20
+            self.height1 = 20
+            self.rotate_amount = 0
+
     def draw(self, screen):
         if self.piece == 'i' or self.piece == 'o':
             pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
-        elif self.piece == 't' or self.piece == 's' or self.piece == 'z':
+        else:
             pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
             pygame.draw.rect(screen, self.color, pygame.Rect(self.x1, self.y1, self.width1, self.height1))
 
     def rotate(self):
         if self.piece == 'i':
-            if self.x >= 240 - self.width:
+            if self.x >= 240 - self.height + 20 or self.x <= 20 - self.height + 20:
                 pass
             else:
                 self.width, self.height = self.height, self.width
+                if self.rotated:
+                    self.x += 20
+                    self.rotated = False
+                else:
+                    self.x -= 20
+                    self.rotated = True
         elif self.piece == 'o':
             pass
         elif self.piece == 't':
@@ -71,26 +97,24 @@ class Piece:
                 pass
             else:
                 self.width, self.height = self.height, self.width
-                if self.rotate_amount == 0:
-                    self.rotate_amount += 1
+                if self.rotate_amount % 4 == 0:
                     self.x -= 20
                     self.x1 -= 20
-                elif self.rotate_amount == 1:
+                elif self.rotate_amount % 4 == 1:
                     self.x += 20
                     self.x1 -= 20
-                    self.rotate_amount += 1
-                elif self.rotate_amount == 2:
+                elif self.rotate_amount % 4 == 2:
                     self.x -= 20
                     self.y1 -= 20
                     self.y += 20
                     self.x1 += 20
-                    self.rotate_amount += 1
-                elif self.rotate_amount == 3:
+                elif self.rotate_amount % 4 == 3:
                     self.y1 += 20
                     self.y -= 20
                     self.x += 20
                     self.x1 += 20
-                    self.rotate_amount = 0
+
+                self.rotate_amount += 1
 
         elif self.piece == 'z':
             if self.x >= 240 - self.height or self.x1 >= 240 - self.height1:
@@ -122,6 +146,62 @@ class Piece:
                     self.x += 20
                     self.rotated = True
 
+        elif self.piece == 'j':
+            if self.x > 240 - self.height or self.x1 > 240 - self.height1:
+                pass
+            # elif 20 + self.height > self.x:
+            #    pass
+            else:
+                self.width, self.height = self.height, self.width
+                self.width1, self.height1 = self.height1, self.width1
+                if self.rotate_amount % 4 == 0:
+                    self.y1 += 20
+                    self.x -= 20
+                elif self.rotate_amount % 4 == 1:
+                    self.y1 += 20
+                    self.x1 -= 20
+                    self.x += 40
+                elif self.rotate_amount % 4 == 2:
+                    self.y1 -= 40
+                    self.y += 20
+                    self.x1 -= 20
+                    self.x -= 40
+                elif self.rotate_amount % 4 == 3:
+                    self.x += 20
+                    self.x1 += 40
+                    self.y -= 20
+
+                self.rotate_amount += 1
+
+        elif self.piece == 'l':
+            if self.x > 240 - self.height or self.x1 > 240 - self.height1:
+                pass
+            # elif 20 + self.height > self.x:
+            #    pass
+            else:
+                self.width, self.height = self.height, self.width
+                self.width1, self.height1 = self.height1, self.width1
+                if self.rotate_amount % 4 == 0:
+                    self.x1 += 20
+                    self.x -= 40
+                    self.y += 20
+                elif self.rotate_amount % 4 == 1:
+                    self.y1 += 40
+                    self.y -= 20
+                    self.x += 20
+                elif self.rotate_amount % 4 == 2:
+                    self.y1 -= 20
+                    self.x1 -= 40
+                    self.x -= 20
+                elif self.rotate_amount % 4 == 3:
+                    self.x += 40
+                    self.x1 += 20
+                    self.y1 -= 20
+                    # self.y -= 40
+
+
+                self.rotate_amount += 1
+
     def move_left(self):
         if self.piece == 'i' or self.piece == 'o':
             if self.x <= 20:
@@ -129,13 +209,7 @@ class Piece:
             else:
                 self.x -= 20
 
-        elif self.piece == 't':
-            if self.x <= 20 or self.x1 <= 20:
-                pass
-            else:
-                self.x -= 20
-                self.x1 -= 20
-        elif self.piece == 's' or self.piece == 'z':
+        else:
             if self.x <= 20 or self.x1 <= 20:
                 pass
             else:
@@ -148,14 +222,7 @@ class Piece:
                 pass
             else:
                 self.x += 20
-        elif self.piece == 't':
-            if self.x1 >= 220 - self.width1 or self.x >= 220 - self.width:
-                pass
-            else:
-                self.x += 20
-                self.x1 += 20
-
-        elif self.piece == 's' or self.piece == 'z':
+        else:
             if self.x1 >= 220 - self.width1 or self.x >= 220 - self.width:
                 pass
             else:
@@ -163,16 +230,15 @@ class Piece:
                 self.x1 += 20
 
     def move_down(self):
-        if self.piece == 't' or self.piece == 's' or self.piece == 'z':
-            if self.y + self.height >= 420 or self.y1 + self.height1 >= 420:
-                self.at_bottom = True
-            else:
-
-                    self.y += 20
-                    self.y1 += 20
         if self.piece == 'i' or self.piece == 'o':
             if self.y + self.height >= 420:
                 self.at_bottom = True
             else:
-                if self.piece == 'i' or self.piece == 'o':
                     self.y += 20
+
+        else:
+            if self.y + self.height >= 420 or self.y1 + self.height1 >= 420:
+                self.at_bottom = True
+            else:
+                    self.y += 20
+                    self.y1 += 20
